c7435b1ef47fd44980c363fdc1168017ea2195e6
Kasper Haepers <kasper.haepers@outlook.be>
2022-02-18 10:38:26 +0100

    smart checking wich open player is actually playing something

--- main.go
+++ main.go
@@ -5,18 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+    "strings"
 )
 
-func main() {
+// returns the current plater based on which players are open
+// and wich player is actually playing something
+func getCurrentPlayer() string {
     cmd := exec.Command("playerctl", "-l")
-
     var out bytes.Buffer
     cmd.Stdout = &out
-
     err := cmd.Run()
-
     if err != nil {
-        log.Fatal(err)
+        log.Fatalf("Error when checking wich player is running: %v", err)
     }
-    fmt.Printf("playerctl output: %q\n", out.String())
+    players := strings.Split(out.String(), "\n")
+    if len(players) > 0 {
+        players = players[:len(players) - 1]
+    }
+    for i := range players {
+        player := players[i]
+        cmd := exec.Command("playerctl", "-p", player, "status")
+        var statusOut bytes.Buffer
+        cmd.Stdout = &statusOut
+        err := cmd.Run()
+        if err != nil {
+            log.Fatalf("error checking wich open player is playing: %v", err)
+        }
+        status := statusOut.String()
+        //fmt.Printf("\n status from %v: %v", player, statusOut.String())
+        // fmt.Printf("the current players: %v\n", players[i])
+        if strings.Contains(status, "Playing") {
+            return player
+        }
+    }
+    return ""
+}
+
+func main() {
+    fmt.Printf("current playing player: %v", getCurrentPlayer())
 }

cd545bb072c411a9657ccfbd6a35866cc466ba09
Kasper Haepers <kasper.haepers@outlook.be>
2022-02-18 09:51:30 +0100

    just basic shell testing and seeing if i can read StdOut in go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func main() {
+    cmd := exec.Command("playerctl", "-l")
+
+    var out bytes.Buffer
+    cmd.Stdout = &out
+
+    err := cmd.Run()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("playerctl output: %q\n", out.String())
+}
